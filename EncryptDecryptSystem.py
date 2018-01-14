a4a45f7d7feb65330f03eaddef36501350ffdac8
PAndaContron <rajtel99@gmail.com>
2018-01-14 07:25:35 -0500

    Added some functionality

--- EncryptDecryptSystem.py
+++ EncryptDecryptSystem.py
@@ -1,12 +1,12 @@
 import hashlib
-import secrets
+import os
 import binascii
 from Cryptodome.Cipher import AES
 
 def pad(text):
-        while len(text) % 8 != 0:
-            text += ' '
-        return text
+    while len(text) % 16 != 0:
+        text += ' '
+    return text
 
 def encryptKey(key, keys):
     encryptedKeys = [k for k in keys]
@@ -19,10 +19,21 @@ def encryptMessage(message, key):
     aes = AES.new(key, AES.MODE_ECB)
     return binascii.hexlify(aes.encrypt(pad(message).encode('utf-8')))
 
+def decrypt(text, key):
+    aes = AES.new(key, AES.MODE_ECB)
+    text = binascii.unhexlify(text)
+    return aes.decrypt(text)
+
 def hashKeys(keys, salt):
     hashedKeys = [s for s in keys]
     i = 0
     for s in hashedKeys:
-        hashedKeys[i] = binascii.hexlify(hashlib.pbkdf2_hmac('sha256', s, salt, 100000))
+        hashedKeys[i] = shaHash(s, salt)
         i = i+1
     return hashedKeys
+
+def shaHash(text, salt):
+    return binascii.hexlify(hashlib.pbkdf2_hmac('sha256', text, salt, 100000))
+
+def generate(length):
+    return os.urandom(length))
--- FileAccessSystem.py
+++ FileAccessSystem.py
@@ -1,5 +1,8 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import binascii
+
+import EncryptDecryptSystem as cryptSystem
 
 scope = ['https://spreadsheets.google.com/feeds']
 creds = ServiceAccountCredentials.from_json_keyfile_name('credentials.json', scope)
@@ -22,8 +25,8 @@ def addAccount(username, passwordHash, salt, key):
         return False
     accountSheet.update_cell(row, 1, username)
     accountSheet.update_cell(row, 2, passwordHash.decode('utf-8','ignore'))
-    accountSheet.update_cell(row, 3, salt.decode('utf-8','ignore'))
-    accountSheet.update_cell(row, 4, key.decode('utf-8','ignore'))
+    accountSheet.update_cell(row, 3, binascii.hexlify(salt).decode('utf-8','ignore'))
+    accountSheet.update_cell(row, 4, binascii.hexlify(key).decode('utf-8','ignore'))
     accountSheet.update_cell(1, 1, row+1)
 
 def removeAccount(username):
@@ -37,7 +40,17 @@ def accountExists(username):
     column = accountSheet.col_values(1)[1:]
     return username in column
 
-def authenticateAccount(username, passwordHash):
+def authenticateAccount(username, password):
     row = accountSheet.col_values(1).index(username)+1
+    passwordHash = cryptSystem.shaHash(password.encode('utf-8'), getSalt(username))
     goodHash = accountSheet.cell(row, 2).value.encode('utf-8')
     return passwordHash == goodHash
+
+def createAccount(username, password):
+    salt = cryptSystem.generate(16)
+    passwordHash = cryptSystem.shaHash(password.encode('utf-8'), salt)
+    key = cryptSystem.generate(16)
+    addAccount(username, passwordHash, salt, key)
+
+def createPost():
+    print('')
